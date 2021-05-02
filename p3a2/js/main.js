2a48d14cadc087903023225c00637076678e19a9
Jennifer Le <doremonngoccute@gmail.com>
2021-05-01 20:40:14 -0400

    x

--- p3a2/js/main.js
+++ p3a2/js/main.js
@@ -4,24 +4,28 @@ $(document).ready(function() {
     randomImage1();
 });
   function randomImage1(){
-    var imgArray = [
-      "image/aegis.jpg",
-      "image/alliance.png",
-      "image/lgd.jpg",
-      "image/nigma.jpg",
-      "image/nigma1.jpg",
-      "image/liquid.jpg",
-      "image/secret.jpg",
-      "image/og.jpg",
-      "image/og1.png"
-    ];
-    var randomNumber = Math.floor(Math.random() * imgArray.length);
-    console.log(randomNumber);
-    var image = imgArray[randomNumber];
-    console.log(imgArray[randomNumber]);
-    console.log(image);
-    $('#aegis').attr('src', image);
+
+
     $('#buttons').click(function() {
-      location.reload(true);
+      var imgArray = [
+        "image/aegis.jpg",
+        "image/alliance.png",
+        "image/lgd.jpg",
+        "image/nigma.jpg",
+        "image/nigma1.jpg",
+        "image/liquid.jpg",
+        "image/secret.jpg",
+        "image/og.jpg",
+        "image/og1.png"
+      ];
+      var randomNumber = Math.floor(Math.random() * imgArray.length);
+      console.log(randomNumber);
+      var image = imgArray[randomNumber];
+      console.log(imgArray[randomNumber]);
+      console.log(image);
+      $('#aegis').attr('src', image);
+
+
     });
+
   }
